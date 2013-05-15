601bd6492f0fb8771b6c4a057ff250c172d0dc2b
ltebean <yucong1118@gmail.com>
2013-05-15 13:32:17 +0800

    improve

--- src/main/java/com/dianping/wizard/config/Configuration.java
+++ src/main/java/com/dianping/wizard/config/Configuration.java
@@ -1,43 +1,22 @@
 package com.dianping.wizard.config;
 
-
-import com.dianping.wizard.exception.WizardExeption;
-import ognl.Ognl;
-import ognl.OgnlException;
-import org.yaml.snakeyaml.Yaml;
-
 import java.io.InputStream;
-import java.util.Map;
 
 /**
  * @author ltebean
  */
 public class Configuration {
 
-    private static  Map<String,Object> config;
+    private static final Yaml yaml;
 
-    static {
-        try {
-            InputStream input = Configuration.class.getResourceAsStream("/wizard.yaml");
-            Yaml yaml = new Yaml();
-            config = (Map<String,Object>)yaml.load(input);
-        } catch(Exception e) {
-            throw new WizardExeption("failed to load configuraion: wizard.yaml",e);
-        }
-
+    static{
+        InputStream input=Configuration.class.getResourceAsStream("/wizard.yaml");
+        yaml=new Yaml(input);
     }
 
-    private static Object getConfig(String expression){
-        try {
-            final Object ognlTree = Ognl.parseExpression(expression);
-            return  Ognl.getValue(ognlTree, config);
-        } catch (OgnlException e) {
-            return null;
-        }
-    }
 
     public static <T> T get(String expression,Class<T> clazz){
-        return (T) getConfig(expression);
+        return yaml.get(expression,clazz);
     }
 
 }
